4c4621547664cc2e2f5dde631e5fa14e2cd1dc31
Jordan Hodge <jordanchasehodge@gmail.com>
2016-02-12 04:57:03 +0000

    committing assignment

--- app.js
+++ app.js
@@ -1,22 +1,50 @@
-var mainModule = angular.module("TempConverter", []);
+var mainModule = angular.module("TempConverter", ['ngAnimate']);
 
 mainModule.controller('ConverterController', function ($scope) {
-    //MainModule.controller('TempConverter');
     
-    var tc = this;
+    //declaring variables
+    var cc = this;
+    cc.tempValue = 0;
+    cc.celsius = 0;
+    cc.fahrenheit = 0;
     
-    tc.celsius = 0;
-    tc.Fahrenheit = 0;
+    //converting degrees from fahrenheit to celsius
+    cc.FtoC = function FtoC() {
+        cc.celsius = (cc.tempValue - 32) / 1.8;
+    };
     
-    tc.FtoC = function FtoC() {
-        tc.celsius = (tc.fahrenheit - 32) / 1.8;
-    }
+    //converting degrees from celsius to fahrenheit
+    cc.CtoF = function CtoF() {
+        cc.fahrenheit = cc.tempValue * 1.8 + 32; 
+    };
+});
+
+mainModule.controller('ButtonController', function ($scope) {
     
-    tc.CtoF = function CtoF() {
-        tc.fahrenheit = tc.celsius * 1.8 + 32; 
-    }
+    //$scope.fahrenheit = false;
+    //$scope.celsius = false;
     
-    //$scope.$watch('tc.Ftemperature', FtoC);
-    //$scope.$watch('tc.tempValue', update);
+    var bc = this;
+                    
+    bc.show_fahrenheit = false;
+   bc.show_celsius = false;
+        
+    bc.show = function show(){
+        bc.show_fahrenheit = true;
+        bc.show_celsius = false;
+    };
+                    
+    bc.hide = function hide(){
+        bc.show_fahrenheit = false;
+        bc.show_celsius = true;
+    }; 
     
+    /*bc.show = function show(){
+        bc.show_celsius = true;
+    };
+                    
+    bc.hide = function hide(){
+        bc.show_celsius = false;
+    };*/  
+                
 });
